da8ef264184ff5b76eb371e047613e4acb865dbd
Roxedus <me@roxedus.dev>
2020-07-18 07:44:20 +0200

    Move website over

---
+++ assets/functions.js
@@ -0,0 +1,5 @@
+function openGH() {
+    const iframe = document.getElementById("gh_collapse_frame");
+
+    iframe.src = "https://ionicabizau.github.io/github-profile-languages/api.html?Roxedus";
+}

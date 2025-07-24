c3cd17c7c56f0cb2fefbe7f9fb21246a720c56dc
Roxedus <me@roxedus.dev>
2025-07-24 17:20:48 +0000

    Use mise

--- site/.hugo-tested.sh
+++ site/.hugo-tested.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 hugoVer="$(hugo version | sed -n -e 's/hugo.v\([[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\)-.*/\1/p')"
+nodeVer="$(node -v)"
+nodeVer="${nodeVer#*v}"
+
+echo "Updating Hugo version to $hugoVer in GitHub workflows"
+echo "Updating Node.js version to $nodeVer in GitHub workflows"
+
 
 for i in .github/workflows/*-hugo*.yml; do
     yq -i "(.jobs.deploy.steps.[] | select( .id == \"install\")).with.hugo-version = \"$hugoVer\"" $i
+    yq -i "(.jobs.deploy.steps.[] | select( .id == \"setup-node\")).with.node-version = \"$nodeVer\"" $i
 done
